30afdfa64524327dcb330ffa4ed18f8ac442e56e
Taimoor <taimooransari072@gmail.com>
2026-04-29 14:10:14 +0500

    commmit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TagManager from "react-gtm-module";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const tagManagerArgs = {
+  gtmId: "GTM-MCG6FQTP",
+};
+
+TagManager.initialize(tagManagerArgs);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
